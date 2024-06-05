5c719c08c93310dcd340040fec63383bddaaf00d
Mario Turtoi <84230374+woolly26@users.noreply.github.com>
2024-06-05 03:31:14 +0300

    Changes and fixes

--- client.h
+++ client.h
@@ -2,5 +2,10 @@
 #define CLIENT_H
 
 void start_client();
+void connect_to_server();
+void send_message(const std::string& message);
+void receive_message();
+void disconnect_from_server();
+
 
 #endif // CLIENT_H
--- server.h
+++ server.h
@@ -2,5 +2,11 @@
 #define SERVER_H
 
 void start_server();
+void create_socket();
+void bind_socket();
+void listen_for_connections();
+void accept_connection();
+void handle_client();
+void close_socket();
 
 #endif // SERVER_H
